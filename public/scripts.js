d67e22bd618a455e28ccd7520b82d8adc0fdf73d
Brandon Porter <bmpgotcha@gmail.com>
2019-08-13 10:42:58 +0100

    Merged the two js files

--- public/scripts.js
+++ public/scripts.js
@@ -2,6 +2,26 @@ function initialize() {
     firebase.initializeApp(firebaseConfig);
 }
 
+function aboutRedirect() {
+    window.location.replace('about.html');
+}
+
+function friendsRedirect() {
+    window.location.replace('friends.html');
+}
+
+function my_scheduleRedirect() {
+    window.location.replace('my_schedule.html');
+}
+
+function settingsRedirect() {
+    window.location.replace('settings.html');
+}
+
+function homeRedirect() {
+    window.location.replace('home.html');
+}
+
 function login() {
     firebase.auth().setPersistence(firebase.auth.Auth.Persistence.LOCAL) //
     .then(function() {
@@ -30,7 +50,7 @@ function login() {
     
             firebase.auth().onAuthStateChanged(function(user) {
                 if (user) {
-                  window.location.replace('home.html');
+                    homeRedirect();
                 }
             });
         })
@@ -62,10 +82,6 @@ function addUser(profile) {
     });
 }
 
-// function redirLogin() {
-
-// }
-
 function logout() {
     firebase.auth().signOut().then(function() {
         // Sign-out successful.
@@ -73,3 +89,37 @@ function logout() {
         // An error happened.
     });
 }
+
+function getUserData() {
+    firebase.initializeApp(firebaseConfig);
+    var user = firebase.auth().currentUser;
+    var name = "User's Name";
+    if(user) {
+        console.log('user signed in');
+        name = user.displayName;
+    }
+
+    document.getElementById("username").innerHTML = name;
+
+    // window.onload = function(){
+    //     // you do not need to initialize like this, but I like to
+    //     var bar1 = new String('placeholder1');
+    //     var bar2 = new String('placeholder2');
+    //     var foo = new Array();
+
+    //     // populate the Array with our Strings 
+    //     foo.push(bar1);
+    //     foo.push(bar2);
+
+    //     // create an array containing all the p tags on the page 
+    //     // (which is this case is only one, would be better to assign an id)
+    //     pArray = document.getElementsByTagName('p');
+
+    //     // create a text node in the document, this is the proper DOM method
+    //     bar1TextNode = document.createTextNode(foo[0].toString());
+
+    //     // append our new text node to the element in question
+    //     pArray[0].appendChild(bar1TextNode);
+    // };
+
+} //ATTEMPTING TO DISPLAY THE USER'S NAME ON HEADER OF WEBSITE
