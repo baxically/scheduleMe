6269b001635bc6ceebfba18ca4c3588060985e20
Kevin Carabajal <44950063+Ghost-Kozak@users.noreply.github.com>
2019-08-13 11:28:17 +0100

    Added further templates to the page. Have now deployed the site
    This is the dummy site for templates
    https://popup-temps.firebaseapp.com

--- Dummy_files/public/scripts.js
+++ Dummy_files/public/scripts.js
@@ -14,3 +14,11 @@ function togglePopup() {
     var popup = document.getElementById("myPopup");
     popup.classList.toggle("show");
 }
+
+function confirmAction() {
+    confirm("This popup notifies user of an action. User is allowed to cancel this action by pressing cancel (Note: it acts as a bool. True when pressing ok, false when pressing cancel)");
+}
+
+function alertAction() {
+    alert("Similar to confirm. But this popup only gives info to user, does not allow a cancel. Only has an ok option");
+}
