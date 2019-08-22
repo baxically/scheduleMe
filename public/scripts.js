41d1839a63d8539a5e4d04d409bf7c1854ed00d8
Chrystal <chrystalmaebagtas@gmail.com>
2019-08-21 18:42:45 -0700

    Dummy data working to show algorithm works

--- public/scripts.js
+++ public/scripts.js
@@ -419,6 +419,91 @@ $(function() { // Chrystal's date picker: once apply is pressed, start and end d
     });
 });
 
+function compareDates( startA, endA, startB, endB ) {
+    // console.log(startA);
+    // console.log(endA);
+    // console.log(startB);
+    // console.log(endB);
+
+	var overlapStart;
+	var overlapEnd;
+	if ( startA >= endB )
+	{
+		//return "No overlap.";
+		return;
+
+	}
+	else if ( startB >= endA )
+	{
+		//return "No overlap.";
+		return;
+	}
+	else if ( startA <= startB && endA >= endB )
+	{
+		//return "Overlap!!!";
+		overlapStart = startB;
+		overlapEnd = endB;
+	}
+	else if ( startA <= startB && endA < endB )
+	{
+		//return "Overlap!!!";
+		overlapStart = startB;
+		overlapEnd = endA;
+	}
+	else if ( startA >= startB && endB <= endA )
+	{
+		//return "Overlap!!!";
+		overlapStart = startA;
+		overlapEnd = endB;
+	}
+	else if ( startA >= startB && endA < endB )
+	{
+		//return "Overlap!!!";
+		overlapStart = startA;
+		overlapEnd = endA;
+    }
+    // console.log(overlapStart);
+    // console.log(overlapEnd);
+
+    //var options = { year: '2-digit', month: '2-digit', day: 'numeric' };
+
+	//return overlapStart.format('YYYY-MM-DD hh:mm A') + " to " + overlapEnd.format('YYYY-MM-DD hh:mm A');
+    return overlapStart.toLocaleDateString() + " to " + overlapEnd.toLocaleDateString();
+}
+
+// var arrStartA = [Date("8/18/19"), Date("8/28/19")];
+// var arrEndA = [Date("8/25/19"), Date("8/29/19")];
+// var arrStartB = [Date("8/19/19")];
+// var arrEndB = [Date("8/21/19")];
+// var commonTimes = "";
+// var allCommonTimes = "";
+function compareFriendsAvailabilities(arrStartA, arrEndA, arrStartB, arrEndB)
+{
+    //alert(arrStartA);
+    var i, j;
+    var commonTimes = "";
+    var allCommonTimes = "";
+    for ( i = 0; i < arrStartA.length; i++)
+    {
+        for ( j = 0; j < arrStartB.length; j++ )
+        {
+            commonTimes = compareDates(arrStartA[i], arrEndA[i], arrStartB[j], arrEndB[j]);
+            console.log(commonTimes);
+            if ( typeof commonTimes === 'undefined')
+            {
+
+            }
+            else
+            {
+                allCommonTimes = allCommonTimes + commonTimes + '\n';
+            }
+        }
+    }
+    console.log(allCommonTimes);
+    alert(allCommonTimes);
+    return allCommonTimes;
+}
+
 //     })
 //     // console.log("New date range selected: " + start.format('M/DD hh:mm A') +
 //     // ' to ' + end.format('M/DD hh:mm A'));
