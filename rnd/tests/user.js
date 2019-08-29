4baab8295a94159f87fe65096458bb9be2d77331
Howard Chen <cchen277@ucr.edu>
2019-08-30 00:31:55 +0100

    deleted userClass() function (firebase related))

--- rnd/tests/user.js
+++ rnd/tests/user.js
@@ -23,27 +23,4 @@ class User {
     }
 };
 
-async function userClass() {
-    var email;
-    var dataPassIn;
-    
-    var user = firebase.auth().currentUser;
-    email = user.email;
-
-    var db = firebase.firestore();
-    userRef = db.collection('users').doc(email);
-    await userRef.get()
-    .then((doc) => {
-        dataPassIn = {
-            email: email,
-            displayName: doc.data().displayName,
-            avatar: doc.data().avatar,
-            events: doc.data().events
-        }
-    }).catch((err) => {console.error("Error getting documents: ", err)})
-    
-    var user_class = new User(dataPassIn.email, dataPassIn.displayName, dataPassIn.avatar, dataPassIn.events);
-    return user_class;
-}
-
 module.exports = User;
